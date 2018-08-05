d0f82ef5bdc5781606eb40a931661324cbc36178
Md Abu Zafar <mdabuzafar78@gmail.com>
2018-08-05 20:12:29 +0530

    Add the index function and redirect user on successful creation of topic

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -4,7 +4,26 @@ defmodule Discuss.TopicController do
   # aliases
   alias Discuss.Topic
 
-  def new(conn, params) do
-    changeset = Topic.changest(%Topic{}, %{})
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render conn, "index.html", topics: topics
+  end
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+    case Repo.insert(changeset) do
+      {:ok, post} -> 
+        conn 
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
+
+      {:error, changeset} -> 
+        render conn, "new.html", changeset: changeset
+    end
   end
 end
