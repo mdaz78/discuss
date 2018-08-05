e670f0b572010e8179de2b994ffcb8a839088662
Md Abu Zafar <mdabuzafar78@gmail.com>
2018-08-05 21:07:45 +0530

    Fix typo

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -17,7 +17,7 @@ defmodule Discuss.TopicController do
   def create(conn, %{"topic" => topic}) do
     changeset = Topic.changeset(%Topic{}, topic)
     case Repo.insert(changeset) do
-      {:ok, post} -> 
+      {:ok, _topic} -> 
         conn 
         |> put_flash(:info, "Topic Created")
         |> redirect(to: topic_path(conn, :index))
