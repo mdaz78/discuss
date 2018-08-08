a20d2974ccef4968b869d13e0fc32c76e96fab04
Md Abu Zafar <mdabuzafar78@gmail.com>
2018-08-08 11:11:23 +0530

    generate the user params to save to the user table

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -2,11 +2,15 @@ defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
 
+  alias Discuss.User
+
   def callback(conn, params) do
-    IO.puts "++++++++++++++++++++++++++++++"
-    IO.inspect conn.assigns
-    IO.puts "++++++++++++++++++++++++++++++"
-    IO.inspect params
-    IO.puts "++++++++++++++++++++++++++++++"
+    auth = conn.assigns.ueberauth_auth
+    %{"provider" => provider} = params
+    user_params = %{
+      token: auth.credentials.token,
+      email: auth.info.email,
+      provider: provider
+    }
   end
 end
