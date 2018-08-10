d66cfa67795015083c659f50460acf419d41b985
Md Abu Zafar <mdabuzafar78@gmail.com>
2018-08-10 10:38:23 +0530

    Make the call function to assign the user to conn

--- web/controllers/plugs/set_user.ex
+++ web/controllers/plugs/set_user.ex
@@ -7,11 +7,19 @@ defmodule Discuss.Plugs.SetUser do
   # aliases
   alias Discuss.Repo
   alias Discuss.User
-  alias Discuss.Router.Helpers
 
   def init(_params) do
   end
 
   def call(conn, _params) do
+    user_id = get_session(conn, :user_id)
+    cond do
+      user = user.id && Repo.get(User, user_id) ->
+        assign(conn, :user, user) 
+        # after this step conn.assigns.user will give us the user construct
+
+      true ->
+        assign(conn, :user, nil)
+    end
   end
 end
