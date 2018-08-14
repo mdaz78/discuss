95ae9727748b7a82f9120fca2cf7d4e51af03a67
Md Abu Zafar <mdabuzafar78@gmail.com>
2018-08-14 10:14:21 +0530

    Add the method to show the details page

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -8,6 +8,11 @@ defmodule Discuss.TopicController do
   plug Discuss.Plugs.RequireAuth when action in[:new, :create, :edit, :update, :delete]
   plug :check_topic_owner when action in[:edit, :update, :delete]
 
+  def show(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    render conn, "show.html", topic: topic
+  end
+
   def index(conn, _params) do
     topics = Repo.all(Topic)
     render conn, "index.html", topics: topics
